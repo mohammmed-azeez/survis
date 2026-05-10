a2b5e2fc5a1ce877499964233c3b02726ba53ead
Mohammed Azeez <85385770+mohammmed-azeez@users.noreply.github.com>
2026-05-10 19:02:54 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,118 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+const generatedBibEntries = {
+    "Jiang2021Applications": {
+        "author": "Jiang, Weiwei",
+        "doi": "10.1016/j.eswa.2021.115537",
+        "journal": "Expert Systems with Applications",
+        "keywords": "type:survey, deep_learning, stock_prediction, LSTM, CNN",
+        "series": "ESWA",
+        "title": "Applications of Deep Learning in Stock Market Prediction: Recent Progress",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
+        "volume": "184",
+        "year": "2021"
+    },
+    "Moghar2020Stock": {
+        "author": "Moghar, Adil and Hamiche, Mhamed",
+        "doi": "10.1016/j.procs.2020.03.049",
+        "journal": "Procedia Computer Science",
+        "keywords": "type:prediction, LSTM, RNN, stock_prediction",
+        "pages": "1168--1173",
+        "series": "Procedia CS",
+        "title": "Stock Market Prediction Using LSTM Recurrent Neural Network",
+        "type": "inproceedings",
+        "volume": "170",
+        "year": "2020"
+    },
+    "Wu2022SILSTM": {
+        "author": "Wu, Shengting and Liu, Yuling and Zou, Ziran and Weng, Tien-Hsiung",
+        "doi": "10.1080/09540091.2021.1940101",
+        "journal": "Connection Science",
+        "keywords": "type:prediction, LSTM, sentiment_analysis, stock_prediction, CNN",
+        "pages": "44--62",
+        "series": "Connection Science",
+        "title": "S_I_LSTM: Stock Price Prediction Based on Multiple Data Sources and Sentiment Analysis",
+        "type": "article",
+        "volume": "34",
+        "year": "2022"
+    },
+    "Kong2025Deep": {
+        "author": "Kong, Xiangjie and Chen, Zhenghao and Liu, Weiyao and Ning, Kaili and Zhang, Lechao and Xia, Feng",
+        "doi": "10.1007/s13042-025-02560-w",
+        "journal": "International Journal of Machine Learning and Cybernetics",
+        "keywords": "type:survey, deep_learning, time_series, forecasting, LSTM, Transformer",
+        "pages": "5079--5112",
+        "series": "IJMLC",
+        "title": "Deep learning for time series forecasting: a survey",
+        "type": "article",
+        "volume": "16",
+        "year": "2025"
+    },
+    "Sang2024Novel": {
+        "author": "Sang, Shuai and Li, Lu",
+        "doi": "10.3390/math12070945",
+        "journal": "Mathematics",
+        "keywords": "type:prediction, LSTM, attention_mechanism, stock_prediction",
+        "number": "7",
+        "series": "Mathematics",
+        "title": "A Novel Variant of LSTM Stock Prediction Method Incorporating Attention Mechanism",
+        "type": "article",
+        "volume": "12",
+        "year": "2024"
+    },
+    "Fischer2018Deep": {
+        "author": "Fischer, Thomas and Krauss, Christopher",
+        "doi": "10.1016/j.ejor.2017.11.054",
+        "journal": "European Journal of Operational Research",
+        "keywords": "type:prediction, LSTM, financial_prediction, deep_learning",
+        "pages": "654--669",
+        "series": "EJOR",
+        "title": "Deep Learning with Long Short-Term Memory Networks for Financial Market Predictions",
+        "type": "article",
+        "volume": "270",
+        "year": "2018"
+    },
+    "Arleo2023Visual": {
+        "author": "Arleo, Alessio and Tsigkanos, Christos and Leite, Roger A. and Dustdar, Schahram and Miksch, Silvia and Sorger, Johannes",
+        "doi": "10.1111/cgf.14723",
+        "journal": "Computer Graphics Forum",
+        "keywords": "type:visualization, visual_analytics, financial_data",
+        "pages": "101--116",
+        "series": "CGF",
+        "title": "Visual Exploration of Financial Data with Incremental Domain Knowledge",
+        "type": "article",
+        "volume": "42",
+        "year": "2023"
+    },
+    "Dong2024Financial": {
+        "author": "Dong, Alice Xiaodan and Huang, Weidong and Wang, Jitong",
+        "doi": "10.1145/3678698.3678702",
+        "journal": "VINCI 2024",
+        "keywords": "type:visualization, financial_visualization, big_data, machine_learning",
+        "series": "VINCI",
+        "title": "Financial Big Data Visualization: A Machine Learning Perspective",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "Ko2016Survey": {
+        "author": "Ko, Sungahn and Cho, Isaac and Afzal, Shehzad and Yau, Calvin and Chae, Junghoon and Malik, Abish and Beck, Kaethe and Jang, Yun and Ribarsky, William and Ebert, David S.",
+        "doi": "10.1111/cgf.12931",
+        "journal": "Computer Graphics Forum",
+        "keywords": "type:survey, financial_data, visualization, visual_analytics",
+        "pages": "599--617",
+        "series": "CGF",
+        "title": "A Survey on Visual Analysis Approaches for Financial Data",
+        "type": "article",
+        "volume": "35",
         "year": "2016"
+    },
+    "Sawhney2020Deep": {
+        "author": "Sawhney, Ramit and Agarwal, Shivam and Wadhwa, Arnav and Shah, Rajiv Ratn",
+        "doi": "10.18653/v1/2020.emnlp-main.676",
+        "journal": "EMNLP 2020",
+        "keywords": "type:prediction, GNN, attention, stock_prediction, social_media",
+        "pages": "8415--8426",
+        "series": "EMNLP",
+        "title": "Deep Attentive Learning for Stock Movement Prediction From Social Media Text and Company Correlations",
+        "type": "inproceedings",
+        "year": "2020"
     }
 };
