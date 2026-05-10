1dbde8c11f0e4de75ae6af52006b8e34b1d48485
Mohammed Azeez <85385770+mohammmed-azeez@users.noreply.github.com>
2026-05-10 19:09:33 +0100

    Update available_img.js

--- src/data/generated/available_img.js
+++ src/data/generated/available_img.js
@@ -1,1 +1,12 @@
-const availableImg = ["Beck2016Visual"];
+const availableImg = [
+    "Jiang2021Applications",
+    "Moghar2020Stock",
+    "Wu2022SILSTM",
+    "Kong2025Deep",
+    "Sang2024Novel",
+    "Fischer2018Deep",
+    "Arleo2023Visual",
+    "Dong2024Financial",
+    "Ko2016Survey",
+    "Sawhney2020Deep"
+];
